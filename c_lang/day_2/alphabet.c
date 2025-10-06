8d8922888dd179827d894895b53a1ec6cbc27163
g1thua <gilbertmwangi254@gmail.com>
2025-10-06 13:14:08 +0300

    printing alphabets using putchar

---
+++ c_lang/day_2/alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char alphabets = 'a';
+
+    while(alphabets < 'z')
+    {
+        putchar(alphabets);
+        alphabets++;
+        
+    }
+    
+    putchar('\n');
+
+    return 0;
+
+
+}
